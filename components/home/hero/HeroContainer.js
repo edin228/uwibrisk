5a2d928b1df28095414cf152b50c6aa350fc69ca
edin228 <edin228@yahoo.com>
2022-08-30 06:10:13 -0700

    new home page

--- components/home/hero/HeroContainer.js
+++ components/home/hero/HeroContainer.js
@@ -3,7 +3,7 @@ import HeroCard from "./HeroCard";
 
 function HeroContainer( {locations = []} ) {
   return (
-    <div className="flex lg:hidden flex-col h-[190px] w-full  z-20">
+    <div className="flex flex-col h-[190px] w-full  z-20">
       <div className="flex overflow-x-auto overflow-y-hidden space-x-2 lg:space-x-0 lg:gap-4 w-full rounded-md">
       {locations.map((card) => (
           <HeroCard
--- components/home/testemonials/TestemonialCard.js
+++ components/home/testemonials/TestemonialCard.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { getIcon } from "../../../utils/utils";
+import { motion } from "framer-motion";
 
 function TestemonialCard({ testemonial }) {
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <div className="absolute bg-teal-500 flex items-center text-center justify-center text-white text-3xl font-bold top-0 left-0 rounded-full w-[50px] h-[50px] z-30 shadow-lg border-2 border-white">
         {getIcon("quoteIcon")}
       </div>
+
       <div className="flex text-xs lg:text-sm flex-col absolute top-5 left-7 z-10  bg-slate-100 w-[300px] h-fit p-4 rounded-2xl shadow-2xl">
+        {testemonial.teamMembers.length > 0 ? (
+          <div className="absolute top-[-15px] right-[-18px]  bg-teal-500 flex items-center text-center justify-center text-white text-3xl font-bold rounded-full w-[45px] h-[45px] z-30 shadow-lg">
+            <motion.img
+              src={testemonial?.teamMembers[0]?.photo?.url}
+              className="h-full w-fit rounded-full shadow-lg border-2 border-white"
+            ></motion.img>
+          </div>
+        ) : null}
         <div className="font-semibold">{testemonial.text}</div>
-        <div className="flex w-full justify-end font-bold mt-4">{testemonial.from}</div>
-        <div className="flex w-full justify-end font-semibold italic opacity-80 text-xs">{testemonial.subText}</div>
+        <div className="flex w-full justify-end font-bold mt-4">
+          {testemonial.from}
+        </div>
+        <div className="flex w-full justify-end font-semibold italic opacity-80 text-xs">
+          {testemonial.subText}
+        </div>
       </div>
     </div>
   );
