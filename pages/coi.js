78f59a4aa18a5b2cde93b5f4b351ddb6af13c3e1
edin228 <edin228@yahoo.com>
2023-09-25 13:49:24 -0700

    fix programs box height

--- pages/coi.js
+++ pages/coi.js
@@ -21,7 +21,7 @@ function COI() {
               href={"https://clientportal.vertafore.com/Login/3004020-1"}
               className="flex flex-col transition duration-100 bg-white border-2 shadow-lg cursor-pointer hover:scale-105 border-zinc-100 rounded-xl xl:min-h-[300px] xl:h-[200px] w-full xl:w-[237px] 2xl:w-[300px]"
             >
-              <div className="flex flex-col items-center justify-center h-full p-2 text-center text-white rounded-xl buring-orange-gradient">
+              <div className="flex flex-col items-center justify-center h-full p-2 text-center text-white rounded-xl black-gradient">
                 <div className="flex p-3 text-5xl border-4 border-white drop-shadow-xl rounded-xl">
                   {getIcon("link")}
                 </div>
@@ -36,7 +36,7 @@ function COI() {
             <div className="flex items-center justify-center w-full h-[100px]">
               <a
                 href={"https://clientportal.vertafore.com/Login/3004020-1"}
-                className="flex items-center justify-center w-full h-full p-4 text-xl font-bold text-white rounded-lg shadow-lg orange-yellow-gradient"
+                className="flex items-center justify-center w-full h-full p-4 text-xl font-bold text-white rounded-lg shadow-lg black-gradient"
               >
                 Manage Your Coverage
               </a>
@@ -47,7 +47,7 @@ function COI() {
               href={"/certificates-of-insurance"}
               className="transition duration-100 hover:scale-105 cursor-pointer flex flex-col bg-white shadow-lg border-2 border-zinc-100 rounded-xl xl:min-h-[300px] xl:h-[200px] w-full xl:w-[237px] 2xl:w-[300px]"
             >
-              <div className="flex flex-col items-center justify-center h-full p-2 text-center text-white rounded-xl orange-yellow-gradient">
+              <div className="flex flex-col items-center justify-center h-full p-2 text-center text-white rounded-xl black-gradient">
                 <div className="flex p-3 text-5xl border-4 border-white drop-shadow-xl rounded-xl">
                   {getIcon("certificate")}
                 </div>
@@ -62,7 +62,7 @@ function COI() {
             <div className="flex items-center justify-center w-full h-[100px]">
               <a
                 href={"https://uwibrisk.com/client-portal-request"}
-                className="relative flex items-center justify-center w-full h-full p-4 text-xl font-bold text-white rounded-lg shadow-lg buring-orange-gradient"
+                className="relative flex items-center justify-center w-full h-full p-4 text-xl font-bold text-white rounded-lg shadow-lg black-gradient"
               >
                 <div className="relative">
                   Apply for Instant COI
