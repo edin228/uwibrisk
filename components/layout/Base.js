92ffa977239ffe77180934c0c238a54a7be70080
Edin Dedovic <edin228@yahoo.com>
2025-09-25 00:28:51 -0700

    updated programs page

--- components/layout/Base.js
+++ components/layout/Base.js
@@ -7,9 +7,9 @@ function Base({ children, template = null, data = null, title=null }) {
   return (
     <div className="relative flex w-full h-full overflow-hidden text-color">
       
-      <div className="absolute top-[20%] right-[0%] z-[-10] w-[600px] h-[100px] bg-yellow-500 skew-y-[-12deg] opacity-30 bg-accent-blur"></div>
-      <div className="absolute top-[8%] right-[0%] z-[-10] w-[300px] h-[60px] bg-yellow-500 skew-y-[-12deg] opacity-30 bg-accent-blur"></div>
-      <div className="absolute top-[16%] right-[0%] z-[-10] w-[400px] h-[20px] bg-black skew-y-[-12deg] opacity-30 bg-accent-blur"></div>
+      <div className="absolute top-[20%] right-[-14%] z-[-10] w-[600px] h-[100px] bg-yellow-500 skew-y-[-12deg] opacity-30 bg-accent-blur"></div>
+      <div className="absolute top-[8%] right-[-14%] z-[-10] w-[300px] h-[60px] bg-yellow-500 skew-y-[-12deg] opacity-30 bg-accent-blur"></div>
+      <div className="absolute top-[16%] right-[-14%] z-[-10] w-[400px] h-[20px] bg-black skew-y-[-12deg] opacity-30 bg-accent-blur"></div>
       <div className="relative flex flex-col w-full h-full overflow-hidden">
         <Header />
         <div className="relative flex flex-col h-full overflow-x-hidden lg:pt-20 pb-10">
